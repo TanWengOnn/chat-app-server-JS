266b23fb8a7deb2b582a91b44837a9ffddbb6510
Tan Weng Onn <weng.onn@besquare.com.my>
2022-07-14 20:56:15 +0800

    chat app

--- public/chat.js
+++ public/chat.js
@@ -5,41 +5,44 @@ var socket = io.connect("http://localhost:5000"); // io.connect("http://192.168.
 var message = $("#message");
 var username = $("#username");
 var btn = $("#send");
-var output  = $("#output");
-var feedback  = $("#feedback");
+var output = $("#output");
+var feedback = $("#feedback");
 
-socket.emit("history", {}, function (response){
-    response.forEach(el => {
-        output.append("<p><strong>" + el.username +":</strong>" + el.text + "</p>");
-    })
-})
+socket.emit("history", {}, function (response) {
+  response.forEach((el) => {
+    output.append(
+      "<p><strong>" + el.username + ":</strong>" + el.text + "</p>"
+    );
+  });
+});
 
+//-----------------------Send Message---------------------//
 //Emit event
 btn.on("click", function () {
-    socket.emit("chat", {
-        username: username.val(),
-        message: message.val()
-    });
+  socket.emit("chat", {
+    username: username.val(),
+    message: message.val(),
+  });
 });
 
 message.keypress(function () {
-    socket.emit("typing", username.val());
+  socket.emit("typing", username.val());
 });
 
 //Listen event
 socket.on("chat", function (data) {
-    output.append("<p><strong>" + data.username +":</strong>" + data.message + "</p>");
-    feedback.html("");
-    document.getElementById("chat-window").scrollTop = output[0].scrollHeight;
+  output.append(
+    "<p><strong>" + data.username + ":</strong>" + data.message + "</p>"
+  );
+  feedback.html("");
+  document.getElementById("chat-window").scrollTop = output[0].scrollHeight;
 });
 
 socket.on("typing", function (data) {
-    // console.log(data);
-    // feedback.append("<p> <em>" + data + " is typing... </em> </p>");
+  console.log(data);
+  feedback.append("<p> <em>" + data + " is typing... </em> </p>");
 
-    // setTimeout(function() {
-    //     feedback.innerHTML = "";
-    // }, 300)
-
+  setTimeout(function () {
+    feedback.innerHTML = "";
+  }, 300);
 });
-
--- server.js
+++ server.js
@@ -1,21 +1,23 @@
+// Importing Libraries in Node.js
 const express = require("express");
 const admin = require("firebase-admin");
 const socket = require("socket.io");
-require('dotenv').config()
+require("dotenv").config();
 
+// Initializing the Web-FrameWork
 const app = express();
 
 var serviceAccount = require("./sdk_secret.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_URL
+  databaseURL: process.env.FIREBASE_URL,
 });
 
-const port =  5000;
+const port = 5000;
 
 const server = app.listen(port, function () {
-    console.log("Listening on port", port);
+  console.log("Listening on port", port);
 });
 
 app.use(express.static("public"));
@@ -23,53 +25,61 @@ app.use(express.static("public"));
 const message_db = admin.database().ref("/messages");
 
 const io = socket(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-      }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on("error", () => {
-    
-})
+io.on("error", () => {});
 
-io.on("connection", socket => {
-    socket.on("chat", data => {
-        io.sockets.emit("chat", data);
-        if (Object.values(data)) {
-            message_db.push({
-                time: Date.now(),
-                username: data.username ? data.username : "",
-                text: data.message? data.message : ""
-            })
-        }
-    });
+io.on("connection", (socket) => {
+  socket.on("chat", (data) => {
+    io.sockets.emit("chat", data);
+    if (Object.values(data)) {
+      message_db.push({
+        time: Date.now(),
+        username: data.username ? data.username : "",
+        text: data.message ? data.message : "",
+      });
+    }
+  });
 
-    socket.on("typing", data => {
-        socket.broadcast.emit("typing", data)
-    });
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing", data);
+  });
 
-    socket.on("history", (params, callback) => {
-        if (params.from && params.to) {
-            message_db.orderByChild("time").startAt(params.from).endAt(params.to).on("value", function(snapshot) {
-                const hist = Object.values(snapshot.val());
-                callback(hist);
-            })
-        } else if (params.from) {
-            message_db.orderByChild("time").startAt(params.from).on("value", function(snapshot) {
-                const hist = Object.values(snapshot.val());
-                callback(hist);
-            })
-        } else if (params.to) {
-            message_db.orderByChild("time").endAt(params.to).on("value", function(snapshot) {
-                const hist = Object.values(snapshot.val());
-                callback(hist);
-            })
-        } else {
-            message_db.on("value", function(snapshot) {
-                const hist = Object.values(snapshot.val());
-                callback(hist);
-            })
-        }
-    })
+  socket.on("history", (params, callback) => {
+    if (params.from && params.to) {
+      message_db
+        .orderByChild("time")
+        .startAt(params.from)
+        .endAt(params.to)
+        .on("value", function (snapshot) {
+          const hist = Object.values(snapshot.val());
+          callback(hist);
+        });
+    } else if (params.from) {
+      message_db
+        .orderByChild("time")
+        .startAt(params.from)
+        .on("value", function (snapshot) {
+          const hist = Object.values(snapshot.val());
+          callback(hist);
+        });
+    } else if (params.to) {
+      message_db
+        .orderByChild("time")
+        .endAt(params.to)
+        .on("value", function (snapshot) {
+          const hist = Object.values(snapshot.val());
+          callback(hist);
+        });
+    } else {
+      message_db.on("value", function (snapshot) {
+        const hist = Object.values(snapshot.val());
+        callback(hist);
+      });
+    }
+  });
 });
